b7d672a00f060648281fb3f3295d963a66ebe031
jimelrod <elrod.development@gmail.com>
2016-07-24 03:35:44 -0500

    added data project

---
+++ KnowYourKnockout.Data/KnowYourKnockoutDataApi.cs
@@ -0,0 +1,40 @@
+﻿using KnowYourKnockout.Data.Models;
+using KnowYourKnockout.Data.Repositories;
+using System;
+using System.Data.Common;
+
+namespace KnowYourKnockout.Data
+{
+    public class KnowYourKnockoutDataApi
+    {
+        public KnowYourKnockoutDataApi()
+        {
+            InitializeRepositories(new KnowYourKnockoutDataContext());
+        }
+
+        public KnowYourKnockoutDataApi(string connectionString)
+        {
+            InitializeRepositories(new KnowYourKnockoutDataContext(connectionString));
+        }
+
+        public KnowYourKnockoutDataApi(DbConnection connection)
+        {
+            InitializeRepositories(new KnowYourKnockoutDataContext(connection));
+        }
+
+        public IRepository<Category, Guid> Categories { get; private set; }
+        public IViewRepository<FriendRelationship> FriendRelationships { get; private set; }
+        public IRepository<FriendRequest, Guid> FriendRequests { get; private set; }
+        public IRepository<Question, Guid> Questions { get; private set; }
+        public IRepository<User, Guid> Users { get; private set; }
+
+        private void InitializeRepositories(KnowYourKnockoutDataContext context)
+        {
+            Categories = new CategoryRepository(context);
+            FriendRelationships = new FriendRelationshipViewRepository(context);
+            FriendRequests = new FriendRequestRepository(context);
+            Questions = new QuestionRepository(context);
+            Users = new UserRepository(context);
+        }
+    }
+}

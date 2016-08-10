6839e9451b8bd48e8520cc3ef70592e371f9e929
jimelrod <elrod.development@gmail.com>
2016-08-10 02:41:02 -0500

    Added BASE testing for calling an API

--- KnowYourKnockout.Web.Ui/src/models/user.js
+++ KnowYourKnockout.Web.Ui/src/models/user.js
@@ -1,3 +1,17 @@
 ﻿export class User {
+    id = '';
+    emailAddress = '';
+    firstName = '';
+    lastName = '';
+    isActive = false;
+    joinedOn = new Date(0,0);
 
+    constructor(obj) {
+        this.id = obj.id;
+        this.emailAddress = obj.emailAddress;
+        this.firstName = obj.firstName;
+        this.lastName = obj.lastName;
+        this.isActive = obj.isActive;
+        this.joinedOn = obj.joinedOn;
+    }
 }
---
+++ KnowYourKnockout.Web.Ui/src/services/userService.js
@@ -0,0 +1,22 @@
+﻿import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-fetch-client';
+import 'fetch';
+
+@inject(HttpClient)
+export class UserService{
+    constructor(http) {
+        http.configure(config => {
+            config
+              .useStandardConfiguration()
+              .withBaseUrl('http://localhost:4753/api');
+        });
+
+        this.http = http;
+    }
+
+    getUser(id) {
+        return this.http.fetch('values/' + id)
+            .then(response => response.json())
+            .then(user => console.log(user));
+    }
+}

0e2f1ee0577e9c8ac03b3dcf40be224f96efa091
jimelrod <elrod.development@gmail.com>
2016-08-10 23:17:07 -0500

    Updated DB deploy scripts

--- KnowYourKnockout.Database/dbo/Scripts/PostDeploy/TagSeedData.sql
+++ KnowYourKnockout.Database/dbo/Scripts/PostDeploy/TagSeedData.sql
@@ -1,10 +1,10 @@
-﻿MERGE INTO [dbo].[Category] AS Target 
+﻿MERGE INTO [dbo].[Tag] AS Target 
 USING (VALUES 
   (CONVERT(uniqueidentifier, '9b3c503d-3601-4272-8ea9-8fc7423babf3'), N'Politics'), 
   (CONVERT(uniqueidentifier, '80414055-89cd-4c80-874e-1284d45a1732'), N'Religion'), 
   (CONVERT(uniqueidentifier, '1437681b-3fc3-4502-9259-c2ebfa0e7596'), N'Education'),
   (CONVERT(uniqueidentifier, 'f8663cbd-1754-4d24-9862-890f6aa33d10'), N'Family'),
-  (CONVERT(uniqueidentifier, '930eb6ac-aaf9-4e11-b765-936c9de473a4'), N'Other')
+  (CONVERT(uniqueidentifier, '930eb6ac-aaf9-4e11-b765-936c9de473a4'), N'Vices')
 ) 
 AS Source (Id, Name) 
 ON Target.Id = Source.Id 

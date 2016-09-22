b157df202fa3f5c6bfdb2129ee4d9feb202919aa
jimelrod <elrod.development@gmail.com>
2016-09-22 08:29:38 -0500

    Base work for using firebase auth

---
+++ KnowYourKnockout.Web.Api/Controllers/SignInController.cs
@@ -0,0 +1,23 @@
+﻿using KnowYourKnockout.Web.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowYourKnockout.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class SignInController : Controller
+    {
+        [HttpPost]
+        public IActionResult SignIn([FromBody]FirebaseUser user)
+        {
+            Console.Write("Pause");
+
+
+
+            return Json(user);
+        }
+    }
+}
---
+++ KnowYourKnockout.Web.Api/Models/FirebaseUser.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace KnowYourKnockout.Web.Api.Models
+{
+    public class FirebaseUser
+    {
+        [JsonProperty(PropertyName = "displayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty(PropertyName = "email")]
+        public string Email { get; set; }
+
+        [JsonProperty(PropertyName = "photoURL")]
+        public string PhotoUrl { get; set; }
+
+        [JsonProperty(PropertyName = "uid")]
+        public string uid { get; set; }
+    }
+}
